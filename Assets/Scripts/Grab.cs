250babe4f9a180a3f7a2dc5efe4f7d4d7e24ffda
nato <natogourmet@hotmail.com>
2020-10-10 17:11:42 -0500

    agarrar

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Grab : MonoBehaviour
+{
+    public bool grabbing = false;
+    public GameObject grabbed;
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyUp(KeyCode.Q) && !grabbing)
+        {
+            if (other.GetComponent<Grabbable>() == null) return;
+            grabbing = true;
+            other.transform.SetParent(transform);
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.transform.localPosition = Vector3.zero;
+            other.enabled = false;
+            grabbed = other.gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && grabbing)
+        {
+            grabbing = false;
+            grabbed.transform.SetParent(null);
+            grabbed.GetComponent<Rigidbody>().isKinematic = false;
+            grabbed.GetComponent<Collider>().enabled = true;
+            grabbed = null;
+        }
+    }
+}
