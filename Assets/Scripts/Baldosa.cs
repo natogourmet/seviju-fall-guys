f342d52787ab0435851f16627f811023631e2ccd
andersonbp13 <andersonbp13@hotmail.com>
2020-10-10 15:57:26 -0500

    salto y colliders

---
+++ Assets/Scripts/Baldosa.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baldosa : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "jugador")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Persona.cs
+++ Assets/Scripts/Persona.cs
@@ -5,38 +5,67 @@ public class Persona : MonoBehaviour
     public int edad;
     public string nombre;
     public float velocidad;
-    
+    public float velocidadGiro;
+    public float magnitudSalto;
+    public bool estaSaltando;
+
+    void Start()
+    {
+        estaSaltando = false;
+    }
+
     void Update()
     {
         Caminar();
+        Saltar();
+        Hablar();
+        
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    void Saltar()
+    {
+        if (!estaSaltando && Input.GetKeyDown(KeyCode.Space))
         {
-            Hablar();
+            estaSaltando = true;
+            transform.GetComponent<Rigidbody>().AddForce(Vector3.up * magnitudSalto, ForceMode.Force);
         }
     }
 
     void Caminar()
     {
-       
-        transform.Translate(new Vector3(0, 0 , 1) * velocidad * Time.deltaTime * Input.GetAxis("Vertical"));
-       
-
-        if (Input.GetKey(KeyCode.A))
+        if (!estaSaltando)
         {
-            transform.Translate(new Vector3(-1, 0, 0) * velocidad * Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, 1) * velocidad * Time.deltaTime * Input.GetAxis("Vertical"));
         }
-
-    
-        if (Input.GetKey(KeyCode.D))
+        else
         {
-            transform.Translate(new Vector3(1, 0, 0) * velocidad * Time.deltaTime);
+            if(Input.GetAxis("Vertical") == 1)
+            {
+                transform.Translate(new Vector3(0, 0, 1) * (velocidad / 2) * Time.deltaTime);
+            }
         }
+        
+        transform.Rotate(new Vector3(0, 1, 0) * velocidadGiro * Time.deltaTime * Input.GetAxis("Horizontal"));
     }
 
     void Hablar()
     {
-        Debug.Log("mi nombre es: " + nombre);
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Debug.Log("mi nombre es: " + nombre);
+        }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "suelo")
+        {
+            estaSaltando = false;
+        }
+        else if (collision.transform.tag == "Respawn") {
+            transform.position = Vector3.zero;
+        }
+    }
+
+    
 }
