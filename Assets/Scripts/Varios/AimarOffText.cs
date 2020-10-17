edda71996bba746d842173901f0848788051eaae
Morion <adrian.martinez@udea.edu.co>
2020-10-17 17:31:33 -0500

    cambios en la escena de la clase 4

---
+++ Assets/Scripts/Varios/AimarOffText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimarOffText : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    public float velocidad = 0.5f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * velocidad;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
